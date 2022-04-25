1fff296221e907e48c230c63b467c66d113dd6b6
Radostin Minchev <tinchoRSM@gmail.com>
2022-04-25 15:17:19 +0300

    Loding movies from api by tittle.

---
+++ client/src/features/movies/moviesSlice.js
@@ -0,0 +1,45 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    movies: [],
+    isLoading: true
+}
+
+const movieUrl = "http://localhost:8080/movies";
+
+export const getMoviesFromApi = createAsyncThunk(
+    'movies/getMoviesFromApi',async (name, thunkAPI) =>{
+        try {
+            const response = await fetch(movieUrl + `/${name}`);
+            const data = await response.json();
+
+            return data;
+
+        } catch (error) {
+            console.log(error)
+        }
+    })
+    
+
+export const moviesSclice = createSlice({
+    name: "movies",
+    initialState: initialState,
+    reducers: {
+
+    },
+    extraReducers:{
+        [getMoviesFromApi.pending]: (state) =>{
+            state.isLoading = true;
+        },
+        [getMoviesFromApi.fulfilled]: (state,action) =>{
+            state.isLoading = false;
+            state.movies = action.payload;
+        },
+        [getMoviesFromApi.rejected]: (state,action) =>{
+            state.isLoading = false;
+        }
+    }
+})
+
+export default moviesSclice.reducer;
