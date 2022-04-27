08e4a7654100c4fd1e92960eb8a3902dea055f50
Radostin Minchev <tinchoRSM@gmail.com>
2022-04-27 17:15:01 +0300

    Added favorites and fixed cors.

--- client/src/components/Landing/LandingFavorites.jsx
+++ client/src/components/Landing/LandingFavorites.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 
 import "../../styles/Landing.css";
+import EmptyFavorites from '../Utility/EmptyFavorites';
 
 export default function LandingFavorites() {
 
@@ -9,18 +10,16 @@ export default function LandingFavorites() {
 
 
   const favoritesElements = favorites.map((el)=>{
-    return <p key={el}>{el}</p>
-    
-  }
-    
+      return <img className="landing--favorite--img" key={el.id} src={el.poster} alt="poster"/>
+    }
   )
 
   return (
     <div className="landing--favorites"> 
         <div className="landing--favorites--container">
-            <h1>Your Favorites</h1>
-            <div>
-              {favoritesElements}
+            <h1 className="landing--favorites--text">Your Favorites</h1>
+            <div className="landing--favorites--elements">
+              {favoritesElements ? favoritesElements : <EmptyFavorites/>}
             </div>
         </div>
     </div>
